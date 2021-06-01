8086808ab394e61a3382a1c9fbe7e7c1f7b3a03d
ledangvu <ledangvu@gmail.com>
2021-06-01 21:12:03 +0700

    Add project files.

---
+++ XamarinWithShell/XamarinWithShell/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinWithShell.ViewModels;
+
+namespace XamarinWithShell.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ XamarinWithShell/XamarinWithShell/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinWithShell.Models;
+using XamarinWithShell.ViewModels;
+
+namespace XamarinWithShell.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
